082cf2f0aee78841dc0cc702503f00ed8b79f3fc
Harold <haroldmedina18@hotmail.com>
2025-01-25 18:41:24 -0500

    init project

---
+++ api-franquicia-pt/src/main/java/com/pt/franquicia/api/api_franquicia_pt/ApiFranquiciaPtApplication.java
@@ -0,0 +1,13 @@
+package com.pt.franquicia.api.api_franquicia_pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFranquiciaPtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFranquiciaPtApplication.class, args);
+	}
+
+}
